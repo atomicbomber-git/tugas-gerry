2f7fc0ec8a5a29508a44ca0d1cec853ae6d0b09c
Antonius Yonathan <james.patrick.keegan.business@gmail.com>
2022-07-18 21:06:27 +0700

    First commit.

---
+++ resources/views/layouts/frontend/master.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<title>Tugas UAS AIK 4</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<style type="text/css" media="all">
+@import "images/style.css";
+</style>
+</head>
+<body>
+<div class="content">
+  <div id="header">
+    <div class="title">
+      <h1>UAS AIK 4</h1>
+      <h3>Nama : Gilbertus Gerry Pratama Irianto</h3>
+      <h3>NIM : 202220110</h3>
+    </div>
+  </div>
+  @yield('content')
+  <div id="prefooter">
+    {{-- <div class="particles">
+      <h2>Top Articles</h2>
+      <img src="images/arrow.gif" alt="" /> <a href="#">Integer euismod ante non diam adipiscing</a> <br />
+      <img src="images/arrow.gif" alt="" /> <a href="#">consectetuer adipiscing elit dolor sit amet</a> <br />
+      <img src="images/arrow.gif" alt="" /> <a href="#">Lorem ipsum dolor sit amet ipsum dolor</a><br />
+      <img src="images/arrow.gif" alt="" /> <a href="#">Integer euismod ante non adipiscing elit </a><br />
+      <img src="images/arrow.gif" alt="" /> <a href="#">Lorem ipsum dolor sit amet euismod ante</a><br />
+    </div>
+    <div class="comments">
+      <h2>Last Comments</h2>
+      <img src="images/arrow.gif" alt="" /> <a href="#">Integer euismod ante non diam adipiscing</a> <br />
+      <img src="images/arrow.gif" alt="" /> <a href="#">consectetuer adipiscing elit dolor sit amet</a> <br />
+      <img src="images/arrow.gif" alt="" /> <a href="#">Lorem ipsum dolor sit amet ipsum dolor</a><br />
+      <img src="images/arrow.gif" alt="" /> <a href="#">Integer euismod ante non adipiscing elit </a><br />
+      <img src="images/arrow.gif" alt="" /> <a href="#">Lorem ipsum dolor sit amet euismod ante</a><br />
+    </div> --}}
+  </div>
+  <div id="footer">
+    {{-- <div class="padding"> Copyright &copy; 2006 Your Site Name | Design: <a href="http://www.free-css-templates.com">David Herreman </a> | <a href="#">Contact</a> | <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> and <a href="http://validator.w3.org/check?uri=referer">XHTML</a> | <a href="http://www.solucija.com">Solucija.com</a> | <a href="#">Login</a> </div> --}}
+  </div>
+</div>
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.frontend.master')
+@section('content')
+    
+
+<div id="main">
+    <div class="center">
+      <h2>Pengertian Akhlak dalam Islam</h2>
+      Akhlak merupakan sebuah sistem yang mengatur tindakan dan pola sikap manusia dalam menjalani kehidupan sehari-hari. Dalam ajaran agama islam, sistem nilai tersebut merupakan sumber ijtihad sebagai salah satu metode berpikir secara islami. Akhlak memicu terjadinya tindakan dan hubungan antara Allah, sesama manusia dan alam semesta.</p>
+      Menurut Imam Al-Ghazali, akhlak merupakan salah satu sifat yang tertanam dalam jiwa manusia yang dapat menimbulkan suatu perbuatan yang mudah dilakukan tanpa adanya pertimbangan pemikiran lagi. Sementara itu, Muslim Nurdin mengatakan bahwa akhlak adalah sebuah sistem nilai yang mengatur tindakan manusia yang ada di muka bumi.</p>
+      Adapun pengertian akhlak menurut Muslim Nurdin dibagi menjadi dua sudut pandang, yaitu Suluq Azzahriah dan Bataniah. Suluq azzhariah merupakan suatu cara pandang yang memperlihatkan hal-hal yang tampak di dalam diri seperti tutur kata, tingkah laku dan watak. Sementara itu menurut sudut pandang Bataniah, akhlak adalah ilmu yang membahas berbagai masalah yang dihadapi manusia terkait dengan hal-hal yang bersifat Kejiwaan.
+      <p class="date"></p>
+      
+      <br />
+      <h2>Macam-Macam Akhlak dalam Islam</h2>
+      Menurut Islam, macam-macam akhlak ada dua yaitu akhlakul mahmudah (akhlak terpuji) dan akhlakul mazmumah (akhlak tercela). Adapun defenisinya sebagai berikut:</p>
+      1. Akhlakul Mahmudah</p>
+      Akhlakul mahmudah atau disebut dengan akhlak yang terpuji merupakan salah satu golongan macam-macam akhlak yang harus dimiliki setiap umat muslim. Adapun contoh macam-macam akhlak tersebut diantarannya sikap rela berkorban, jujur, sopan, santun, tawakal, adil, sabar dan lain sebagainya. Sebagai umat muslim sudah seharusnya kita selalu menjaga akhlakuk karimah dalam menjalani kehidupan sehari-hari.</p>
+      2. Akhlakul Mazmumah</p>
+      Akhlakul mazmumah atau akhlak tercela merupakan salah satu tindakan buruk yang harus dihindari setiap manusia. Hal ini harus dijauhi karena akhlakul mazmumah dapat mendatangkan mudharat bagi diri sendiri maupun orang lain. Contoh dari macam-macam akhlakul mazmumah yaitu sombong, iri, dengki, takabur, aniaya, ghibah dan lain sebagainya. Sebagai orang muslim sudah seharusnya kita menghindari akhlakuk mazmumah atau akhlak tercela.
+      <p class="date"></p>
+      <br />
+
+      <h2>Manfaat Akhlakul Mahmudah</h2>
+      Setiap muslim dianjurkan untuk memiliki akhlakul mahmudah atau akhlak yang terpuji. Bagi seseorang yang memiliki sikap tersebut maka dapat mendatangkan manfaat bagi kehidupan sehari-hari maupun di akhirat nanti.</p>
+      Berikut ini beberapa manfaat macam akhlak terpuji:</p>
+      1. Dicintai Nabi Muhammad SAW</p>
+      Keutamaan memiliki akhlakul karimah yang pertama ialah dicintai Rasulullah SAW. Disebutkan dalam sebuah hadis, seorang muslim yang memiliki sifat terpuji maka menjadi orang yang dekat dengan Nabi Muhammad SAW. Sebagaimana dalam hadits berikut ini, Rasulullah saw bersabda:</p>
+      “Orang yang paling saya cintai dan paling dekat dengan tempat saya kelak di hari kiamat adalah mereka yang memiliki akhlak mulia. Sementara orang yang paling saya benci dan tempatnya paling jauh dari saya kelak di hari kiamat adalah mereka yang keras dan rakus, suka menghina dan sombong.” (HR. Tirmizi).</p>
+      2. Berat Timbangannya di Hari Kiamat</p>
+      Seorang muslim yang memiliki sikap akhlakul karimah di hari akhir kelak akan diselamatkan oleh Allah SWT. Selain itu, setiap muslim yang memiliki akhlakul karimah juga dapat mencapai derajat seperti seseorang yang berpuasa dan salat. Hal ini sebagaimana dalam sebuah hadis, Rasulullah SAW bersabda:</p>
+      “Tidak ada sesuatu amalan yang jika diletakkan dalam timbangan lebih berat dari akhlak yang mulia. Sesungguhnya orang yang berakhlaq mulia bisa menggapai derajat orang yang rajin puasa dan rajin shalat.” (HR. Tirmidzi).</p>
+      3. Mendapat Jaminan Surga</p>
+      Seseorang yang berakhlakul karimah mendapatkan jaminan dari Rasulullah akan mendapatkan sebuah rumah di surga. Dari Abu Umamah ra; Rasulullah SAW bersabda:</p>
+      "Saya menjamin sebuah rumah tepi surga bagi orang meninggalkan debat sekalipun ia benar, dan sebuah rumah di tengah surga bagi orang yang tidak berbohong sekalipun hanya bergurau, dan rumah di atas surga bagi orang yang mulia akhlaknya." (HR Abu Daud)</p>
+      4. Mendapat Kedudukan Tinggi di Akhirat</p>
+      Di akhirat kelak, seorang muslim yang pada masa hidupnya berakhlakul karimah akan mendapatkan kedudukan yang tinggi karena akhlak dan budi pekerti yang ia miliki. Dalam sebuah hadits, Rasulullah bersabda:</p>
+      "Tidak ada kemelaratan yang lebih parah dari kebodohan dan tidak ada harta (kekayaan) yang lebih bermanfaat dari kesempurnaan akal. Tidak ada kesendirian yang lebih terisolir dari ujub (rasa angkuh) dan tidak ada tolong-menolong yang lebih kokoh dari musyawarah. Tidak ada kesempurnaan akal melebihi perencanaan (yang baik dan matang) dan tidak ada kedudukan yang lebih tinggi dari akhlak yang luhur. Tidak ada wara’ yang lebih baik dari menjaga diri (memelihara harga dan kehormatan diri), dan tidak ada ibadah yang lebih mengesankan dari tafakur (berpikir), serta tidak ada iman yang lebih sempurna dari sifat malu dan sabar." (HR. Ibnu Majah dan Ath-Thabrani)
+      <p class="date"></p>
+      <br />
+      <h3> Sumber artikel : <a href="https://hot.liputan6.com/read/4720126/macam-macam-akhlak-dalam-islam-beserta-pengertian-dan-manfaatnya">Liputan6.com</a>
+    </div>
+    {{-- <div class="leftmenu">
+      <div class="nav">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Articles</a></li>
+          <li><a href="#">Gallery</a></li>
+          <li><a href="#">Affiliates</a></li>
+          <li><a href="#">Archives</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </div> --}}
+  </div>
+
+  @stop
